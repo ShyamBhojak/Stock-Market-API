fd7cb70d7af40e60a7a43c9e6bb12c7cf5b8789f
H.B.INSTITUTE - THE TRAINING CENTER <hbinstitute@H.B.INSTITUTE>
2026-09-01 11:43:49 +0530

    Created Model - User with registering in __init__.py and main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from app.core.database import engine
+from app.core.base import Base
+from app.models import User
 
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(
