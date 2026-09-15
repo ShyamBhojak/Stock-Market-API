b0142b77774cc8a07c01f84b98a7769bd5fc8b77
H.B.INSTITUTE - THE TRAINING CENTER <hbinstitute@H.B.INSTITUTE>
2026-09-15 11:59:50 +0530

    Fetching specific stock

--- app/api/routes/stock.py
+++ app/api/routes/stock.py
@@ -5,6 +5,8 @@ from app.core.database import get_db
 from app.models.stock import Stock
 from app.schemas.stock import StockCreate, StockResponse
 
+from fastapi import HTTPException
+
 router = APIRouter(prefix="/stocks",tags=["Stocks"])
 
 @router.post("/createstock", response_model=StockResponse)
@@ -29,4 +31,13 @@ def getstocks(db:Session = Depends(get_db)):
     return {
         "status":"All Stocks",
         "Stocks": stocks
+    }
+@router.get("/{stock_id}")
+def getstocks(stock_id: int, db:Session = Depends(get_db)):
+    stock = db.query(Stock).get(stock_id)
+    if not stock:
+        return HTTPException(404,f"Stock not found at id: {stock_id}")
+    return{
+        "status":"Stock Found",
+        "stock":stock
     }
